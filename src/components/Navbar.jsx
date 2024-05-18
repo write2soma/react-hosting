8e6ad51f01219fa530f8da107940ac4d78d97e82
Soma Dutta <165060922+write2soma@users.noreply.github.com>
2024-05-18 23:09:45 +0530

    hosting first react project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
